fe86b2b5549b37bb48b5b2f4b598884816a60c91
obiomaokorowu <109354887+obiomaokorowu@users.noreply.github.com>
2025-07-27 12:04:52 +0100

    Create iam_role.tf

---
+++ iam_role.tf
@@ -0,0 +1,51 @@
+data "aws_region" "current" {}
+data "aws_partition" "current" {}
+data "aws_caller_identity" "current" {}
+
+resource "aws_flow_log" "this" {
+  iam_role_arn    = aws_iam_role.vpc_flow_log.arn
+  log_destination = "arn:${data.aws_partition.current.partition}:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:${aws_cloudwatch_log_group.vpc_flow_log.name}:*"
+  traffic_type    = "ALL"
+  vpc_id          = module.myapp-vpc.vpc_id
+}
+
+resource "aws_cloudwatch_log_group" "vpc_flow_log" {
+  name              = "/aws/vpc-flow-log/${module.myapp-vpc.vpc_id}"
+  retention_in_days = 30
+}
+
+resource "aws_iam_role" "vpc_flow_log" {
+  name = "vpc-flow-log-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "vpc-flow-logs.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "vpc_flow_log" {
+  name = "vpc-flow-log-policy"
+  role = aws_iam_role.vpc_flow_log.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogGroups",
+          "logs:DescribeLogStreams"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
