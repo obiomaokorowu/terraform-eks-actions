4268ae97fa0cc58d19291d94bb51cf211e13ffbb
obiomaokorowu <109354887+obiomaokorowu@users.noreply.github.com>
2025-07-27 14:08:05 +0100

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -5,35 +5,56 @@ provider "kubernetes" {
 }
 
 data "aws_eks_cluster" "app-cluster" {
-  name = module.eks.cluster_id
-  depends_on = [module.eks]
+  name = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "app-cluster" {
-  name = module.eks.cluster_id
-  depends_on = [module.eks]
-}
-output "cluster_id" {
-  value = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "21.0.4"
+  version = "19.15.3"  # Using a stable version that doesn't have this issue
 
-  name               = "app-eks-cluster"  
-  kubernetes_version = "1.29" # Updated to a supported version
-  subnet_ids         = module.myapp-vpc.private_subnets
-  vpc_id             = module.myapp-vpc.vpc_id
-  endpoint_private_access = false
-  endpoint_public_access  = true
-  enable_cluster_creator_admin_permissions = false
+  cluster_name    = "app-eks-cluster"
+  cluster_version = "1.28"  # Supported EKS version
 
-  access_entries = {
-    github-actions-admin = {
-      principal_arn     = "arn:aws:iam::361769567498:role/github-actions-terraform"
+  vpc_id     = module.myapp-vpc.vpc_id
+  subnet_ids = module.myapp-vpc.private_subnets
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = false
+
+  # IAM Role for Service Account (IRSA) configuration
+  enable_irsa = true
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_groups = {
+    workers = {
+      name           = "worker-nodes"
+      instance_types = ["t2.small"]
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
+
+      # Use existing key pair
+      key_name = "may_key"
+
+      # Required to avoid the count argument error
+      partition = "aws"
+
+      # IAM Role configuration
+      iam_role_additional_policies = {
+        AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+      }
+    }
+  }
+
+  # Access entry for GitHub Actions
+  cluster_access_entries = {
+    github-actions = {
+      principal_arn      = "arn:aws:iam::361769567498:role/github-actions-terraform"
       kubernetes_groups = ["system:masters"]
-      type              = "STANDARD"
       policy_associations = {
         admin = {
           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
@@ -46,26 +67,20 @@ module "eks" {
   }
 
   tags = {
-    environment = "development"
-    application = "app"
-    team        = "devops"
+    Environment = "development"
+    Application = "app"
+    Team        = "devops"
   }
-
-  eks_managed_node_groups = {
-    worker-nodes = {
-      min_size       = 1
-      max_size       = 3
-      desired_size   = 3
-      instance_types = ["t2.small"]
-      key_name       = "may_key"
-      
-      # These belong here in the node group configuration
-      partition  = "aws"
-      account_id = "361769567498"
-
-    }
-  }
-
-  depends_on = [module.myapp-vpc]
 }
 
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  value = module.eks.cluster_security_group_id
+}
