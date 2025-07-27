4f307af647f3796c9c31ff6cad918cc341eb49d0
obiomaokorowu <109354887+obiomaokorowu@users.noreply.github.com>
2025-07-27 14:29:41 +0100

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,84 +1,50 @@
+provider "kubernetes" {
+    #load_config_file = "false"
+    host = data.aws_eks_cluster.app-cluster.endpoint
+    token = data.aws_eks_cluster_auth.app-cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.app-cluster.certificate_authority.0.data)
+}
+
+data "aws_eks_cluster" "app-cluster" {
+    name = module.eks.cluster_name
+    depends_on = [module.eks]
+}
+
+
+data "aws_eks_cluster_auth" "app-cluster" {
+    name = module.eks.cluster_name
+    depends_on = [module.eks]
+}
+output "cluster_id" {
+  value = data.aws_eks_cluster.app-cluster.id
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.10.0" # Latest as of July 2024
+  version = "20.26.0" 
 
-  cluster_name                   = "app-eks-cluster"
-  cluster_version                = "1.30"
-  cluster_endpoint_public_access = true
+  cluster_name = "app-eks-cluster"
+  cluster_version = "1.30"
 
-  vpc_id     = module.myapp-vpc.vpc_id
   subnet_ids = module.myapp-vpc.private_subnets
-
-  # Modern auth configuration 
+  vpc_id = module.myapp-vpc.vpc_id
+  cluster_endpoint_public_access = true
   enable_cluster_creator_admin_permissions = true
 
-  # Alternative legacy auth mapping (still supported)
-  manage_aws_auth_configmap = true
-  aws_auth_users = [
-    {
-      userarn  = data.aws_caller_identity.current.arn
-      username = element(split("/", data.aws_caller_identity.current.arn), length(split("/", data.aws_caller_identity.current.arn)) - 1)
-      groups   = ["system:masters"]
-    }
-  ]
-
-  # Modern addon management
-  cluster_addons = {
-    coredns    = { most_recent = true }
-    kube-proxy = { most_recent = true }
-    vpc-cni    = { 
-      most_recent    = true
-      configuration_values = jsonencode({
-        env = {
-          ENABLE_POD_ENI = "true"
-        }
-      })
-    }
+  tags = {
+    environment = "development"
+    application = "app"
   }
 
   eks_managed_node_groups = {
-    workers = {
-      name         = "worker-nodes"
+    dev = {
       min_size     = 1
       max_size     = 3
-      desired_size = 2
+      desired_size = 3
 
-      instance_types = ["t3.small"] 
-      capacity_type  = "ON_DEMAND"
+      instance_types = ["t2.small"]
       key_name       = "may_key"
-
-      # Modern launch template
-      create_launch_template = true
-      launch_template_tags  = {
-        Name = "worker-nodes"
-      }
     }
   }
-
-  tags = {
-    Terraform   = "true"
-    Environment = "development"
-  }
-}
-
-# Supporting resources
-data "aws_caller_identity" "current" {}
-
-# Kubernetes provider configuration
-data "aws_eks_cluster" "this" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "this" {
-  name = module.eks.cluster_name
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
-}
-
-output "kubeconfig" {
-  value = "aws eks --region ${var.region} update-kubeconfig --name ${module.eks.cluster_name}"
+depends_on = [module.myapp-vpc]
 }
